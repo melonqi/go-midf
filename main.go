8cfc1a11f245618df48ac16f9b48c82251aabc83
melonqi <melonqi@tencent.com>
2019-02-23 15:23:21 +0800

    Update: add set mif header in write

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"go-midf/midf"
-
-	"github.com/go-spatial/geom"
 )
 
 func main() {
 	mif := midf.NewMif()
-	mif.Read("C_IndoorRegion", false)
-	for _, obj := range mif.Objects {
-		fmt.Println(obj.Attributes)
-		switch obj.Geo.(type) {
-		case *geom.Point:
-			fmt.Println("*Point")
-		case *geom.Line:
-			fmt.Println("*Line")
-		case *geom.MultiLineString:
-			fmt.Println("*MultiLineString")
-		case *geom.MultiPolygon:
-			fmt.Println("*MultiPolygon")
-		case *geom.Extent:
-			fmt.Println("*Extent")
+	mif.Read("C_IndoorRegion")
+	mif.Write("test")
+	/*
+		for _, obj := range mif.Objects {
+			fmt.Println(obj.Attributes)
+			switch obj.Geo.(type) {
+			case *geom.Point:
+				fmt.Println("*Point")
+			case *geom.Line:
+				fmt.Println("*Line")
+			case *geom.MultiLineString:
+				fmt.Println("*MultiLineString")
+			case *geom.MultiPolygon:
+				fmt.Println("*MultiPolygon")
+			case *geom.Extent:
+				fmt.Println("*Extent")
+			}
 		}
-	}
+	*/
 	// fmt.Println(midf.Split("1,2,3", ',', '"'))
 	// fmt.Println(midf.Split("1,2,", ',', '"'))
 	// fmt.Println(midf.Split("1,\"2,3\"", ',', '"'))

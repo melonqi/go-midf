357ea569e44e0d3d8894e81fcaf850b6081b8755
melonqi <melonqi@tencent.com>
2019-02-25 22:59:42 +0800

    Update: set mif geo data

--- midf/mif_write.go
+++ midf/mif_write.go
@@ -3,6 +3,9 @@ package midf
 import (
 	"fmt"
 	"os"
+	"strconv"
+
+	"github.com/go-spatial/geom"
 )
 
 func (mif *Mif) Write(fileName string) bool {
@@ -33,8 +36,53 @@ func (mif *Mif) setMif(fileName string) int {
 		fmt.Println("set mif header failed")
 		return -1
 	}
-
+	mif.setMifData(mifFile)
 	return 0
+}
+
+func (mif *Mif) setMifData(file *os.File) {
+	for i := 0; i < len(mif.Objects); i++ {
+		if mif.Objects[i].Geo == nil {
+			file.WriteString("None\n")
+		} else {
+			switch mif.Objects[i].Geo.(type) {
+			case *geom.Point:
+				pt := mif.Objects[i].Geo.(*geom.Point)
+				file.WriteString("Point " + strconv.FormatFloat(pt[0], 'g', 12, 64))
+				file.WriteString(" " + strconv.FormatFloat(pt[1], 'g', 12, 64) + "\n")
+				file.WriteString("\n")
+			case *geom.MultiLineString:
+				multiLineString := mif.Objects[i].Geo.(*geom.MultiLineString).LineStrings()
+				partSize := len(multiLineString)
+				file.WriteString("Pline MULTIPLE " + strconv.Itoa(partSize) + "\n")
+				for j := 0; j < partSize; j++ {
+					ptSize := len(multiLineString[j])
+					file.WriteString("    " + strconv.Itoa(ptSize) + "\n")
+					for k := 0; k < ptSize; k++ {
+						file.WriteString(strconv.FormatFloat(multiLineString[j][k][0], 'g', 12, 64))
+						file.WriteString(" ")
+						file.WriteString(strconv.FormatFloat(multiLineString[j][k][1], 'g', 12, 64))
+						file.WriteString("\n")
+					}
+				}
+				file.WriteString("    Pen (1,2,0)\n")
+			case *geom.MultiPolygon:
+				multiPolygon := mif.Objects[i].Geo.(*geom.MultiPolygon).Polygons()
+				partSize := len(multiPolygon)
+				file.WriteString("Region " + strconv.Itoa(partSize) + "\n")
+				for j := 0; j < partSize; j++ {
+					ptSize := len(multiPolygon[j][0])
+					file.WriteString("  " + strconv.Itoa(ptSize) + "\n")
+					for k := 0; k < ptSize; k++ {
+						file.WriteString(strconv.FormatFloat(multiPolygon[j][0][k][0], 'g', 12, 64))
+						file.WriteString(" ")
+						file.WriteString(strconv.FormatFloat(multiPolygon[j][0][k][1], 'g', 12, 64))
+						file.WriteString("\n")
+					}
+				}
+			}
+		}
+	}
 }
 
 func (mif *Mif) setMid(fileName string) int {
